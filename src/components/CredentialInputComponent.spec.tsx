977cb1857cfa70ca2f99436dd7917b774ca00f10
masterPN <69779985+masterPN@users.noreply.github.com>
2024-02-12 23:49:35 +0700

    impl unit test for error

--- src/components/CredentialInputComponent.spec.tsx
+++ src/components/CredentialInputComponent.spec.tsx
@@ -68,4 +68,108 @@ describe("test CredentialInput component", () => {
       expect(axios.request).toHaveBeenCalledWith(mockConfig);
     });
   });
+
+  it("submits form - sending post api failed with message string", async () => {
+    const { getByLabelText, getByText } = render(<CredentialInputComponent />);
+    const mockError = {
+      message: "Unauthorized",
+    };
+    axios.request.mockRejectedValue(mockError);
+    fireEvent.change(getByLabelText(/host/i), {
+      target: {
+        value: "host",
+      },
+    });
+    fireEvent.change(getByLabelText(/port/i), {
+      target: {
+        value: 0,
+      },
+    });
+    fireEvent.change(getByLabelText(/environment/i), {
+      target: {
+        value: "sys0",
+      },
+    });
+    fireEvent.change(getByLabelText(/username/i), {
+      target: {
+        value: "username",
+      },
+    });
+    fireEvent.change(getByLabelText(/password/i), {
+      target: {
+        value: "password",
+      },
+    });
+
+    fireEvent.click(getByText(/execute/i));
+
+    const mockConfig = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://host:0/ae/api/v1/0/executions",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+      },
+      data: '{"object_name":"SCRI.EXECUTE.WITH.JSON.INPUT.JSON","execution_option":"execute"}',
+    };
+
+    await waitFor(() => {
+      expect(axios.request).toHaveBeenCalledWith(mockConfig);
+    });
+  });
+
+  it("submits form - sending post api failed with message string", async () => {
+    const { getByLabelText, getByText } = render(<CredentialInputComponent />);
+    const mockError = {
+      response: {
+        data: {
+          message: "Unauthorized",
+        },
+      },
+    };
+    axios.request.mockRejectedValue(mockError);
+    fireEvent.change(getByLabelText(/host/i), {
+      target: {
+        value: "host",
+      },
+    });
+    fireEvent.change(getByLabelText(/port/i), {
+      target: {
+        value: 0,
+      },
+    });
+    fireEvent.change(getByLabelText(/environment/i), {
+      target: {
+        value: "sys0",
+      },
+    });
+    fireEvent.change(getByLabelText(/username/i), {
+      target: {
+        value: "username",
+      },
+    });
+    fireEvent.change(getByLabelText(/password/i), {
+      target: {
+        value: "password",
+      },
+    });
+
+    fireEvent.click(getByText(/execute/i));
+
+    const mockConfig = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://host:0/ae/api/v1/0/executions",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Basic dXNlcm5hbWU6cGFzc3dvcmQ=",
+      },
+      data: '{"object_name":"SCRI.EXECUTE.WITH.JSON.INPUT.JSON","execution_option":"execute"}',
+    };
+
+    await waitFor(() => {
+      expect(axios.request).toHaveBeenCalledWith(mockConfig);
+    });
+  });
 });
