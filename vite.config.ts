1baa0b7a5a345fd34af54d2dcb3104380381f037
masterPN <69779985+masterPN@users.noreply.github.com>
2024-02-17 17:24:03 +0700

    exclude unrelated test

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,9 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     css: true,
+    coverage: {
+      provider: "v8",
+      exclude: ["*.config.js", ".eslintrc.cjs", "src/*.*"],
+    },
   },
 });
